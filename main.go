609af1a00a697234d5faa68def9583d23400a773
root <verda>
2021-11-13 14:39:15 +0800

    1-part 2

--- main.go
+++ main.go
@@ -1,18 +1,33 @@
 package main
 
 import (
+	"blog/api/controller"
+	"blog/api/repository"
+	"blog/api/routes"
+	"blog/api/service"
 	"blog/infrastructure"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
+	"blog/models"
 )
 
+func init() {
+	infrastructure.LoadEnv()
+}
+
 func main() {
-	router := gin.Default() //new gin router initialization
-	router.GET("/", func(context *gin.Context) {
-		infrastructure.LoadEnv()     //loading env
-		infrastructure.NewDatabase() //new database connection
-		context.JSON(http.StatusOK, gin.H{"data": "Hello World !"})
-	})
-	router.Run(":8000")
+	router := infrastructure.NewGinRouter()                     //router has been initialized and configured
+	db := infrastructure.NewDatabase()                          // databse has been initialized and configured
+	postRepository := repository.NewPostRepository(db)          // repository are being setup
+	postService := service.NewPostService(postRepository)       // service are being setup
+	postController := controller.NewPostController(postService) // controller are being set up
+	postRoute := routes.NewPostRoute(postController, router)    // post routes are initialized
+	postRoute.Setup()                                           // post routes are being setup
+	db.DB.AutoMigrate(&models.Post{})                           // migrating Post model to datbase table
+	router.Gin.Run(":8000")                                     //server started on 8000 port
+	// router := gin.Default() //new gin router initialization
+	// router.GET("/", func(context *gin.Context) {
+	// 	infrastructure.LoadEnv()     //loading env
+	// 	infrastructure.NewDatabase() //new database connection
+	// 	context.JSON(http.StatusOK, gin.H{"data": "Hello World !"})
+	// })
+	// router.Run(":8000")
 }
